c4911879482c29bdf5395cef1662b5b6db3f0866
Davi Ribeiro <daviribeiro@MacBook-Pro-de-Davi.local>
2020-11-16 18:49:21 -0300

    ajust

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './home.css';
 
+import logoTranscender from '../../assets/LOGO_luz.png'
+import { Button, Language } from '../../components';
+import { history } from '../../utils/helpers';
+
 export default () => {
+  const textDefault = require(`${Language()}`)
+
+  const comeceSeuCerimonial = () =>{
+    history.push('/celebracao')
+  }
+  
   return (
     <div className='box-home'>
-      home
+      <div className='home-text'>
+        <h1>
+          <img src={logoTranscender} title={textDefault.titulo} />
+        </h1>
+        <h4>{textDefault.subtitulo}</h4>
+        <p>{textDefault.conteudo}</p>
+        <Button color='primary' action={()=> comeceSeuCerimonial()}>{textDefault.comeceSeuCerimonial}</Button>
+      </div>
     </div>
     );
 }
